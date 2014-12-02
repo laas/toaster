de6098e2af1d55dac407ca7d4fd3111d229a627e
Gregoire Milliez <gregoire@milliez.fr>
2014-12-02 13:40:52 +0100

    Adapt the agent message to fit new toaster-lib agent.

--- PDG/src/MorseHumanReader.cpp
+++ PDG/src/MorseHumanReader.cpp
@@ -60,28 +60,19 @@ void MorseHumanReader::updateHuman(tf::TransformListener &listener, int humId, s
 
 //TODO: full human case
 /*static void MorseHumanReader::humanJointStateCallBack(const sensor_msgs::JointState::ConstPtr& msg){
-  humanConf.dofNb = 50;
-  if(msg->position.size() > humanConf.dofNb){
-    ROS_ERROR("Nb Dof error: Poster max nbDof %d < Ros nbDof %d", humanConf.dofNb, msg->position.size());
-  return;
+  if(m_LastConfig[101] != NULL){
+      m_LastConfig[101]->skeleton[torso] =   msg->position[13]; //TorsoX
+      m_LastConfig[101]->skeleton[head] =  - msg->position[20]; //Head
+      m_LastConfig[101]->skeleton[r_elbow] = msg->position[18]; //Right elbow
+      m_LastConfig[101]->skeleton[r_wrist] = msg->position[19]; //Right wrist
+      m_LastConfig[101]->skeleton[l_shoulder] = - msg->position[14]; //Left shoulder
+      m_LastConfig[101]->skeleton[l_elbow] = msg->position[15]; //Left elbow
+      m_LastConfig[101]->skeleton[l_wrist] = msg->position[16]; //Left wrist
+      m_LastConfig[101]->skeleton[r_hip] =   msg->position[24]; //Right hip
+      m_LastConfig[101]->skeleton[r_knee] =  msg->position[25]; //Right knee
+      m_LastConfig[101]->skeleton[r_ankle] = msg->position[22]; //Right ankle
+      m_LastConfig[101]->skeleton[l_hip] =   msg->position[10]; //Left hip
+      m_LastConfig[101]->skeleton[l_knee] =  msg->position[11]; //Left knee
+      m_LastConfig[101]->skeleton[l_ankle] = msg->position[5]; //Left ankle
   }
-  humanConf.dof[14] = msg->position[13]; //TorsoX
-  humanConf.dof[15] = - msg->position[20]; //Head
-  humanConf.dof[18] = 1.39626; //Shoulder Right
-humanConf.dof[19] = 0.10472; //Shoulder Right
-humanConf.dof[20] = -0.346273; //Shoulder Right
-humanConf.dof[22] = msg->position[18]; //Right elbow
-humanConf.dof[24] = msg->position[19]; //Right wrist
-humanConf.dof[27] = -1.39626; //Left shoulder
-humanConf.dof[28] = - msg->position[14]; //Left shoulder
-humanConf.dof[29] = - 0.174533; //Left shoulder
-humanConf.dof[31] = msg->position[15]; //Left elbow
-humanConf.dof[33] = msg->position[16]; //Left wrist
-humanConf.dof[37] = msg->position[24]; //Right hip
-humanConf.dof[39] = msg->position[25]; //Right knee
-humanConf.dof[40] = msg->position[22]; //Right ankle
-humanConf.dof[44] = msg->position[10]; //Left hip
-humanConf.dof[46] = msg->position[11]; //Left knee
-humanConf.dof[47] = msg->position[5]; //Left ankle
 }*/
-
