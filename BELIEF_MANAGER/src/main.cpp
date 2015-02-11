722ac8bbdbe2e544b9936f0b248808ccc1057e2c
Gregoire Milliez <gregoire@milliez.fr>
2015-02-11 18:43:11 +0100

    Going further in belief management

--- BELIEF_MANAGER/src/main.cpp
+++ BELIEF_MANAGER/src/main.cpp
@@ -5,7 +5,6 @@
  * Created on February 5, 2015, 2:49 PM
  */
 
-
 #include "SPAR/PDGFactReader.h"
 #include "SPAR/PDGObjectReader.h"
 #include "PDG/FactList.h"
@@ -19,15 +18,18 @@ static std::map<unsigned int, PDG::FactList> factListMap_;
 // Agents with monitored belief
 static std::map<std::string, unsigned int> agentsTracked_;
 
+// Fact is being updated
+static bool factUpdating_ = false;
+
+static unsigned int mainAgentId_ = 1;
+
 // When adding a fact to an agent, the confidence may decrease as 
 // the other's belief are suppositions based on observation
-
 
 bool RemoveFactToAgent(unsigned int myFactId, unsigned int agentId) {
     factListMap_[agentId].factList.erase(factListMap_[agentId].factList.begin() + myFactId);
     return true;
 }
-
 
 bool RemoveFactToAgent(PDG::Fact myFact, unsigned int agentId) {
     bool removed = false;
@@ -90,15 +92,15 @@ bool addFactToAgent(PDG::Fact myFact, double confidenceDecrease, unsigned int ag
 bool addFact(BELIEF_MANAGER::AddFact::Request &req,
         BELIEF_MANAGER::AddFact::Response & res) {
     // Add safely the fact to main agent
-    addFactToAgent(req.fact, 1.0, 1);
+    addFactToAgent(req.fact, 1.0, mainAgentId_);
     // If fact is observable, add it to agents model if in same room.
     // alternative, add it only if agent has visibility => do this only for update
-    if (req.fact.factObservability > 0) {
+    if (req.fact.factObservability > 0.0) {
         // TODO: for each agent present and in same room
         for (std::map<std::string, unsigned int>::iterator it = agentsTracked_.begin(); it != agentsTracked_.end(); ++it) {
             // TODO: if present and in same room
 
-            // TODO: if fact target is not visible, decrease confidence by 50%?
+            // TODO: if fact subject is not visible, decrease confidence by 50%?
 
             addFactToAgent(req.fact, req.fact.factObservability, it->second);
         }
@@ -114,6 +116,9 @@ int main(int argc, char** argv) {
     // Set this in a ros service
     ros::init(argc, argv, "BELIEF_MANAGER");
     ros::NodeHandle node;
+
+    //Initialisation boolean
+    bool init = true;
 
     //Data reading
     PDGFactReader factRdSpark(node, "SPARK/factList");
@@ -146,6 +151,66 @@ int main(int argc, char** argv) {
     /************************/
 
     while (node.ok()) {
+        //Collect facts as 1st input and put in map
+        if (init) {
+            factUpdating_ = true;
+            // First we feed the mainAgent belief state
+            for (unsigned int i = 0; i < factRdSpar.lastMsgFact.factList.size(); i++) {
+                addFactToAgent(factRdSpar.lastMsgFact.factList[i], 1.0, mainAgentId_);
+            }
+
+            for (unsigned int i = 0; i < factRdSpark.lastMsgFact.factList.size(); i++) {
+                addFactToAgent(factRdSpark.lastMsgFact.factList[i], 1.0, mainAgentId_);
+            }
+
+            for (unsigned int i = 0; i < factRdAM.lastMsgFact.factList.size(); i++) {
+                addFactToAgent(factRdAM.lastMsgFact.factList[i], 1.0, mainAgentId_);
+            }
+
+            // Then, if an agent is in same room, we give him the same belief
+            // This is a 1st choice, it may (should?) be thought further
+            for (std::map<std::string, unsigned int>::iterator it = agentsTracked_.begin(); it != agentsTracked_.end(); ++it) {
+
+                // TODO: if present and in same room
+                // TODO: if fact subject is not visible, decrease confidence by 0.3
+                // if in same room and 0.1 otherwise.
+                for (unsigned int i = 0; i < factListMap_[1].factList.size(); i++) {
+                    addFactToAgent(factListMap_[1].factList[i], factListMap_[1].factList[i].factObservability, it->second);
+                }
+            }
+            init = false;
+            factUpdating_ = false;
+        } else {
+
+
+            //compare new fact to previous state.
+
+
+            //update facts if needed in all models
+            factUpdating_ = true;
+            for (unsigned int i = 0; i < factRdSpar.lastMsgFact.factList.size(); i++) {
+                addFactToAgent(factRdSpar.lastMsgFact.factList[i], 1.0, mainAgentId_);
+            }
+
+            for (unsigned int i = 0; i < factRdSpark.lastMsgFact.factList.size(); i++) {
+                addFactToAgent(factRdSpark.lastMsgFact.factList[i], 1.0, mainAgentId_);
+            }
+
+            for (unsigned int i = 0; i < factRdAM.lastMsgFact.factList.size(); i++) {
+                addFactToAgent(factRdAM.lastMsgFact.factList[i], 1.0, mainAgentId_);
+            }
+            
+            // TODO: multi agent:
+            // Update if:
+            // 1) Agent has visibility on fact subject
+            // 2) fact is observable
+            
+            // If fact is not observable, the supervisor will have to add manually
+            // the fact to the agent. (If the robot tell something to human).            
+            
+            factUpdating_= false;
+        }
+
         fact_pub.publish(factList_msg);
 
         ros::spinOnce();
