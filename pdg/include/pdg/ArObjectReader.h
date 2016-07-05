fad407cc4c9f51a94a6264294f1e7134d7959146
J-Paul Marcade <jpmarcad@laas.fr>
2016-07-05 15:17:49 +0200

    Creation of the structure for ArObjectReader class + implementation of constructor
    implementation of ArObjectReader class
    
    implementation of arobjectreader class
    
    toaster with arobjectReader - untested version
    
    delete comment

---
+++ pdg/include/pdg/ArObjectReader.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   ArObjectReader.h
+ * Author: JPaul Marcade
+ *
+ * Created on July, 2016
+ */
+
+#ifndef AROBJECTREADER
+#define	AROBJECTREADER
+
+//This class reads topic from Ar_track_alvar and converts data into toaster-lib type.
+
+#include "ObjectReader.h"
+#include <ros/ros.h>
+#include "tf/transform_listener.h"
+#include <string>
+#include "geometry_msgs/PoseStamped.h"
+#include "tf/transform_listener.h"
+#include <math.h>
+#include <sys/time.h>
+#include <ostream>
+#include "visualization_msgs/Marker.h"
+
+class ArObjectReader : public ObjectReader {
+	
+public:
+    ArObjectReader(ros::NodeHandle& node, std::string topicAR);
+
+private:
+    ros::Subscriber subAR_;
+    
+    void CallbackObj(const visualization_msgs::Marker::ConstPtr& msg);   
+};
+
+#endif	/* AROBJECTREADER */
+
