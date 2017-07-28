a3c2d52eaa60b0c288c1527f2df27502ae6c5bad
Guillaume Sarthou <gsarthou@laas.fr>
2017-07-28 11:11:54 +0200

    move xml function to utility

--- pdg/src/main.cpp
+++ pdg/src/main.cpp
@@ -20,7 +20,6 @@
 
 // Facts
 
-
 // Message generated class
 #include <toaster_msgs/FactList.h>
 #include <toaster_msgs/RobotListStamped.h>
@@ -33,12 +32,11 @@
 #include "tf/transform_datatypes.h"
 #include "std_msgs/String.h"
 
-// Parse XML
-#include <tinyxml.h>
-#include "ros/package.h"
-
 //tf
 #include <tf/transform_broadcaster.h>
+
+//Xml
+#include "pdg/utility/XmlUtility.h"
 
 bool humanFullConfig_ = true; //If false we will use only position and orientation
 bool robotFullConfig_ = true; //If false we will use only position and orientation
@@ -68,110 +66,6 @@ Entity newPoseEnt_("");
 
 // Service client
 ros::ServiceClient* setPoseClient_;
-
-std::vector<std::string> loadPropertiesFromXml(std::string objectID) {
-    std::vector<std::string> ret;
-
-    std::stringstream pathIoTObj;
-    TiXmlDocument listIoTObj;
-    pathIoTObj << ros::package::getPath("pdg") << "/params/iot_objects.xml";
-    listIoTObj = TiXmlDocument(pathIoTObj.str());
-
-    if (!listIoTObj.LoadFile()) {
-        ROS_WARN_ONCE("Error while loading xml file");
-        ROS_WARN_ONCE("error #%d: %s", listIoTObj.ErrorId(), listIoTObj.ErrorDesc());
-    }
-
-    TiXmlHandle hdl(&listIoTObj);
-    TiXmlElement *object_elem = hdl.FirstChild("objects").FirstChild("object").Element();
-
-    std::string id;
-    while (object_elem) { //for each element object
-        id = object_elem->Attribute("id");
-        if (id == objectID) {
-            TiXmlHandle hdl(object_elem);
-            TiXmlElement *fact_elem = hdl.FirstChild("fact").Element();
-
-            std::string fact_name;
-
-            while (fact_elem) { // for each fact
-                fact_name = fact_elem->Attribute("name");
-                // we add the fact name to the returned vector
-                ret.push_back(fact_name);
-                fact_elem = fact_elem->NextSiblingElement();
-            }
-        }
-        object_elem = object_elem->NextSiblingElement();
-    }
-    return ret;
-}
-
-std::string loadValueFromXmlAsString(std::string objectID, std::string factName, std::string objectValueName, std::string objectValue) {
-    ROS_INFO("loadValueFromXmlAsString objectID:%s factName:%s objectValueName:%s objectValue:%s", objectID.c_str(), factName.c_str(), objectValueName.c_str(), objectValue.c_str());
-    std::string ret;
-
-    std::stringstream pathIoTObj;
-    TiXmlDocument listIoTObj;
-    pathIoTObj << ros::package::getPath("pdg") << "/params/iot_objects.xml";
-    listIoTObj = TiXmlDocument(pathIoTObj.str());
-
-    if (!listIoTObj.LoadFile()) {
-        ROS_WARN_ONCE("Error while loading xml file");
-        ROS_WARN_ONCE("error #%d: %s", listIoTObj.ErrorId(), listIoTObj.ErrorDesc());
-    }
-
-    TiXmlHandle hdl(&listIoTObj);
-    TiXmlElement *object_elem = hdl.FirstChild("objects").FirstChild("object").Element();
-
-    std::string id;
-
-    while (object_elem) { //for each element object
-        id = object_elem->Attribute("id");
-        if (id == objectID) {
-            TiXmlHandle hdl(object_elem);
-            TiXmlElement *fact_elem = hdl.FirstChild("fact").Element();
-
-            std::string fact_name;
-
-            while (fact_elem) { // for each fact
-                fact_name = fact_elem->Attribute("name");
-                if (fact_name == factName) {
-                    TiXmlHandle hdl(fact_elem);
-                    TiXmlElement *value_elem = hdl.FirstChild("value").Element();
-
-                    std::string object_value_name;
-                    std::string operation;
-                    std::string object_value;
-                    std::string fact_value;
-                    while (value_elem) { // for each value
-                        object_value_name = value_elem->Attribute("object_value_name");
-                        operation = value_elem->Attribute("operation");
-                        if (value_elem->Attribute("object_value")) {
-                            object_value = value_elem->Attribute("object_value");
-                        }
-                        if (value_elem->Attribute("fact_value")) {
-                            fact_value = value_elem->Attribute("fact_value");
-                        }
-                        if (objectValueName == object_value_name) {
-                            if (operation == "copy") {
-                                ret = objectValue;
-                            }
-                            else if (operation == "equals") {
-                                if (object_value == objectValue) {
-                                    ret = fact_value;
-                                }
-                            }
-                        }
-                        value_elem = value_elem->NextSiblingElement();
-                    }
-                }
-                fact_elem = fact_elem->NextSiblingElement();
-            }
-        }
-        object_elem = object_elem->NextSiblingElement();
-    }
-    return ret;
-}
 
 void fillValue(MovableIoTObject* srcObject, toaster_msgs::Object& msgObject) {
     msgObject.value = srcObject->getValue();
